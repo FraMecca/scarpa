576a0dfecd295860d4c84ff2b4a7525e18bbc04e
Francesco Mecca <me@francescomecca.eu>
2018-12-08 19:46:14 +0100

    fix

--- source/magic.d
+++ source/magic.d
@@ -16,6 +16,22 @@ extern (C) {
 nothrow:
 
 alias magic_t = void*;
+enum
+    MAGIC_NONE              = 0x000000, /// No flags
+    MAGIC_DEBUG             = 0x000001, /// Turn on debugging
+    MAGIC_SYMLINK           = 0x000002, /// Follow symlinks
+    MAGIC_COMPRESS          = 0x000004, /// Check inside compressed files
+    MAGIC_DEVICES           = 0x000008, /// Look at the contents of devices
+    MAGIC_MIME_TYPE         = 0x000010, /// Return the MIME type
+    MAGIC_CONTINUE          = 0x000020, /// Return all matches
+    MAGIC_CHECK             = 0x000040, /// Print warnings to stderr
+    MAGIC_PRESERVE_ATIME    = 0x000080, /// Restore access time on exit
+    MAGIC_RAW               = 0x000100, /// Don't translate unprintable chars
+    MAGIC_ERROR             = 0x000200, /// Handle ENOENT etc as real errors
+    MAGIC_MIME_ENCODING     = 0x000400, /// Return the MIME encoding
+    MAGIC_APPLE = 0x000800; /// Return the Apple creator
+
+enum MAGIC_MIME = MAGIC_MIME_TYPE | MAGIC_MIME_ENCODING;
 
 magic_t magic_open(int flags);
 void magic_close(magic_t ms);
