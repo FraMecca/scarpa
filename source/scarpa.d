46fe3028d2ce6201d52f3faab0362e5612389760
Francesco Mecca <me@francescomecca.eu>
2018-12-12 14:00:40 +0100

    events.d

--- source/scarpa.d
+++ source/scarpa.d
@@ -15,12 +15,21 @@ events:
 3. translation links -> files
 4. save to disk
 */
-void main()
+void main(string[] args)
 {
     import std.range;
+
+    if(!parseCli(args))
+        return;
+    enableLogging(config.log);
+
+    warning(config.projdir);
+
 	Event[] list;
-	Event req = RequestEvent("http://fragal.eu", "test/");
+	Event req = RequestEvent("http://fragal.eu");
 	list ~= req;
+
+    auto db = createDB("prova.db");
 
     while(!list.empty){
         auto ev = list.front; list.popFront;
@@ -28,152 +37,3 @@ void main()
     }
 }
 
-struct Base {
-	private immutable string m_projdir;
-	immutable ID m_parent;
-	immutable ID m_uuid;
-	bool resolved = false;
-
-	this(const string projdir, const ID parent, const UUID uuid) @safe
-	{
-        m_projdir = projdir;
-        m_parent = parent;
-        m_uuid = uuid;
-	}
-
-	this(const string projdir, const UUID parent, const UUID uuid) @safe
-	{
-        m_projdir = projdir;
-        m_parent = parent;
-        m_uuid = uuid;
-	}
-
-    @property const string projdir() @safe { return m_projdir; }
-    @property const ID uuid() @safe { return m_uuid; }
-    @property const ID parent() @safe { return m_parent; }
-}
-
-struct RequestEvent {
-
-	private immutable string m_url;
-    Base base;
-    alias base this;
-
-	this(const string url, const string projdir, const ID parent = ID()) @safe
-	{
-        base = Base(projdir, parent, md5UUID(url));
-		m_url = url;
-	}
-
-	Event[] resolve() @safe
-	{
-		Event[] res;
-		mixin(scopeInvariant);
-
-		requestUrl(m_url).match!(
-			(ReceiveAsRange stream) => res.append(ToFileEvent(stream, m_url, projdir, uuid)),
-			(string raw) => res.append(HTMLEvent(raw, m_url, projdir, uuid))
-			);
-
-		return res;
-	}
-
-    string toString() @safe { return "RequestEvent(basedir: " ~ projdir ~ ", url: " ~ m_url ~ ")"; }
-}
-
-
-struct HTMLEvent {
-
-	private immutable string m_content;
-	private immutable string m_rooturl;
-    Base base;
-    alias base this;
-
-	this(const string content, const string root, const string projdir, const UUID parent) @safe
-	{
-        base = Base(projdir, parent, md5UUID(root ~ content));
-		m_content = content;
-        m_rooturl = root; // the url of the page requested
-	}
-
-	Event[] resolve() @trusted// TODO safe 
-	{
-        import arrogant;
-		mixin(scopeInvariant);
-
-		Event[] res;
-
-        auto arrogante = Arrogant();
-   		auto tree = arrogante.parse(m_content);
-
-		// TODO other tags and js and css
-        foreach(ref node; tree.byTagName("a")){
-            if(!node["href"].isNull){
-                auto tup = parseUrl(node["href"].get(), m_rooturl);
-                res.append(RequestEvent(tup.url, projdir, parent));
-                node["href"] = tup.fname; // replace with a filename on disk
-            }
-        }
-		string s = tree.document.innerHTML;
-        res.append(ToFileEvent(s, m_rooturl, projdir, parent));
-		return res;
-	}
-
-    string toString() @safe { return "HTMLEvent(basedir: " ~ projdir ~ ", rooturl: " ~ m_rooturl ~ ")"; }
-}
-
-
-struct ToFileEvent
-{
-	private SumType!(ReceiveAsRange, string) m_content;
-    private immutable string m_rooturl;
-    private immutable string m_fname;
-    Base base;
-    alias base this;
-
-	this(T)(T content, const string url, const string projdir, const ID parent) @safe
-		if(is(T == string) || is(T == ReceiveAsRange))
-	{
-		m_content = content;
-        m_rooturl = url;
-		m_fname = projdir ~ url.toFileName;
-        base = Base(projdir, parent, md5UUID(m_fname));
-	}
-
-	Event[] resolve() @trusted
-	{
-        import std.algorithm.iteration : each;
-        import std.file : exists, isDir;
-        import std.string : representation;
- 		mixin(scopeInvariant);
-		Event[] res;
-
-        string fname = m_fname.dup;
-
-		logWarn(fname);
-
-		fname.makeDir();
-
-		if(fname.exists) {
-			m_fname.cond!(
-				f => f.isDir, (f) { fname = handleDirExists(f); },
-				{ throw new Exception("Special file"); }
-				);
-		}
-
-		m_content.match!(
-				(string s) {
-					auto fp = File(fname, mode!"w");
-					fp.write(s.representation);
-					},
-				(ReceiveAsRange r) {
-					auto fp = File(fname, mode!"wb");
-					r.each!((e) => fp.write(e));
-					// TODO check file type in case of binary
-					}
-				);
-        return res;
-	}
-
-    string toString() @safe { return "ToFileEvent(basedir: " ~ projdir ~ ", file: " ~ m_fname ~ " url:" ~ m_rooturl ~ ")"; }
-}
